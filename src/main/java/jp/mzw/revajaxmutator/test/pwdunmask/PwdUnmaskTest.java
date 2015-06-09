7e2bd7170974744eb06ae67e05485b544bc31d99
Yuta Maezawa <maezawa@nii.ac.jp>
2015-06-09 18:37:58 +0900

    add PwdUnmaskTest

---
+++ src/main/java/jp/mzw/revajaxmutator/test/pwdunmask/PwdUnmaskTest.java
@@ -0,0 +1,52 @@
+package jp.mzw.revajaxmutator.test.pwdunmask;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import jp.mzw.revajaxmutator.test.WebAppTestBase;
+
+public class PwdUnmaskTest extends WebAppTestBase {
+
+	static String PATH_TO_FILE;
+	static String UPDATE_PASSWD;
+
+	static String PAPER_SELECT_ID 	= "paper";
+	static String PAPER_PASSWD_ID 	= "pwd";
+	static String PAPER_SUBMIT_ID 	= "submit";
+	static String RESULT_ID 		= "result";
+	static String UNMASKCHB_ID 		= "pwdunmask";
+
+	static String SUCCESS_RESULT 	= "Your paper was successfully submitted.";
+
+	private static void readConfig() throws IOException {
+		InputStream is = PwdUnmaskTest.class.getClassLoader().getResourceAsStream("pwdunmask.properties");
+		Properties config = new Properties();
+		config.load(is);
+		
+		URL = config.getProperty("url") != null ? config.getProperty("url") : "" ;
+		PATH_TO_FILE = config.getProperty("path_to_file") != null ? config.getProperty("path_to_file") : "";
+		UPDATE_PASSWD = config.getProperty("update_passwd") != null ? config.getProperty("update_passwd") : "";
+	}
+
+    @BeforeClass
+    public static void launchBrowser() throws IOException {
+    	WebAppTestBase.launchBrowser();
+    	readConfig();
+    }
+
+    @Test
+    public void testSubmit() {
+    	driver.findElement(By.id(PAPER_SELECT_ID)).sendKeys(PATH_TO_FILE);
+    	driver.findElement(By.id(PAPER_PASSWD_ID)).sendKeys(UPDATE_PASSWD);
+    	driver.findElement(By.id(PAPER_SUBMIT_ID)).click();
+    	assertEquals(SUCCESS_RESULT, driver.findElement(By.id(RESULT_ID)).getText());
+    }
+
+}
